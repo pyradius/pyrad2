3fed2895a1372af1eb53d6e3797a88504fcd6c73
Nicholas Amorim <nicholas@santos.ee>
2026-06-17 09:22:54 +0300

    Removing test file

--- scenarios/under_uvloop.py
+++
@@ -1,6 +0,0 @@
-import asyncio, functools, runpy, sys
-import uvloop
-
-asyncio.run = functools.partial(asyncio.run, loop_factory=uvloop.new_event_loop)
-runpy.run_path(sys.argv[1], run_name="__main__")
-
